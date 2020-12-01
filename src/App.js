39002dfce63515e47449100302cf76ce13315f66
Carlos Martinez <curlosmart@gmail.com>
2020-11-30 19:02:58 -0500

    Add weather info

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import {fetchCurrentWeatherData} from './api/';
 import CityInput from './components/CityInput/CityInput'
+import WeatherInfo from './components/WeatherInfo/WeatherInfo'
 
 const App = () => {
 
-  const [city, setCity] = useState('')
-  const [finalCity, setFinalCity] = useState('')
+  const [city, setCity] = useState('');
+  const [finalCity, setFinalCity] = useState('');
+  const [data, setData] = useState({});
 
   useEffect(() => {
     const fetchAPI = async () => {
       const weatherData = await fetchCurrentWeatherData(finalCity);
-      console.log(weatherData)
+      setData(weatherData)
       return weatherData
     }
 
@@ -24,7 +26,7 @@ const App = () => {
   return(
     <div>
       <CityInput handleCityChange={handleCityChange} city={city} setFinalCity={setFinalCity}/>
-      <WeatherInfo />
+      <WeatherInfo cityInfo={data.data}/>
     </div>
   )
 }
--- src/components/WeatherInfo/WeatherInfo.js
+++ src/components/WeatherInfo/WeatherInfo.js
@@ -1,5 +1,25 @@
-const WeatherInfo = () => {
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
 
+const WeatherInfo = ({cityInfo}) => {
+
+  if(!cityInfo) {
+    return null;
+  }
+
+  const icon = cityInfo.weather[0].icon
+
+  console.log(cityInfo)
+
+  return (
+    <div>
+      <div>
+        {cityInfo.name} {cityInfo.weather[0].description}
+      </div>
+
+      <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt='alt'/>
+    </div>
+  )
 }
 
 export default WeatherInfo
