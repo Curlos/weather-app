35f0557999c361ba553f2c612a4e618eac2a964a
Carlos Martinez <curlosmart@gmail.com>
2021-01-07 19:59:50 -0500

    Added charts to display forecasts in temp

--- src/components/WeatherInfo/WeatherInfo.js
+++ src/components/WeatherInfo/WeatherInfo.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios'
 import {Tabs, Tab, Table} from 'react-bootstrap'
+import { AreaChart, Area, ComposedChart, Tooltip, Legend, CartesianGrid, Bar, Line, XAxis, YAxis } from "recharts";
+
 
 const kelvinToFarenheit = (tempKelvin) => {
   return (Math.round((tempKelvin - 273.15) * (9/5) + 32))
@@ -29,16 +31,34 @@ const WeatherInfo = ({cityInfo, forecast}) => {
   console.log('Forecast: ')
   console.log(forecast)
 
+  console.log('FORECAST DATA OBJECT:')
+
+  const data = forecast.list.map((day) => {
+    return {timeDate: day.dt_txt, temp: kelvinToFarenheit(day.main.temp)}
+  });
+  
+
   return (
     <div>
 
       <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt='alt'/>
 
+      <ComposedChart width={730} height={250} data={data}>
+        <XAxis dataKey="timeDate" />
+        <YAxis yAxisId={1} orientation="right" label={{ value: 'Rain mm', angle: -90 }} />
+        <YAxis yAxisId={2} label={{ value: 'Temp (℉)', angle: -90}} />
+        <Tooltip />
+        <Legend />
+        <CartesianGrid stroke="#f5f5f5" />
+        <Line yAxisId={2} type="monotone" dataKey="temp" stroke="#ff0000" />
+      </ComposedChart>
+
       <Tabs defaultActiveKey="current" id="uncontrolled-tab-example">
         <Tab eventKey="current" title="Current Weather">
-        <div>
-              {cityInfo.name} {cityInfo.weather[0].description}
-            </div>
+        
+          <div>
+              <b>Location: </b>{cityInfo.name} <b>Temperature: </b> {kelvinToFarenheit(cityInfo.main.temp)} ℉ <b>Weather: </b> {cityInfo.weather[0].main} ({cityInfo.weather[0].description}) <img src={`http://openweathermap.org/img/wn/${cityInfo.weather[0].icon}@2x.png`} alt='alt'/>
+          </div>
         </Tab>
         <Tab eventKey="forecast" title="Forecast">
           <Table striped bordered hover>
@@ -48,7 +68,7 @@ const WeatherInfo = ({cityInfo, forecast}) => {
                 console.log(day)
                 return (
                     <li key={i}>
-                      {day.dt_txt}, <b>Temperature: </b> {kelvinToFarenheit(day.main.temp)} ℉ <b>Weather: </b> {day.weather[0].main} ({day.weather[0].description}) <img src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} alt='alt'/>
+                      <b>Time: </b>{day.dt_txt}, <b>Temperature: </b> {kelvinToFarenheit(day.main.temp)} ℉ <b>Weather: </b> {day.weather[0].main} ({day.weather[0].description}) <img src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} alt='alt'/>
                     </li>
                 )
             })}
